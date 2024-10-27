82111e84782d871bbfe7e54585a563d424b513f2
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-10-27 18:20:21 +0530

    Refactor Spotify.js file to update Spotify authentication endpoint and client ID, and add new scopes

--- spotify-clone/src/spotify.js
+++ spotify-clone/src/spotify.js
@@ -3,3 +3,11 @@ export const authEndpoint = "https://accounts.spotify.com/authorize";
 const redirectUri = "http://localhost:3000/";
 
 const clientId = "039e89525ab4401486d3c31237f6373b";
+
+const scopes = [
+    "user-read-currently-playing",
+    "user-read-recently-played",
+    "user-read-playback-speed",
+    "user-top-read",
+    "user-modify-playback-state",
+];
