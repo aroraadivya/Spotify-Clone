327f543c1b464f837cbdb5afb7093fc4725f03fb
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-10-27 16:11:03 +0530

    Refactor Login component to fix broken image path

--- spotify-clone/src/Login.js
+++ spotify-clone/src/Login.js
@@ -5,7 +5,7 @@ function Login() {
     return (
         <div className='login'>
             <h1>I'm the login page</h1>
-            <img src="spotifyLogo.png" alt=""/>
+            <img src="images/spotifyLogo.png" alt=""/>
             <button>LOGIN WITH SPOTIFY</button>
         </div>
     );
