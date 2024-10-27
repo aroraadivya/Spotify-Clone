67622f9209b484d785f21139b1a0dbd3f9950b7a
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-10-27 15:36:32 +0530

    Add Login component and associated CSS file

--- spotify-clone/src/Login.js
+++ spotify-clone/src/Login.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import './Login.css'
 
 function Login() {
     return (
-        <div>
+        <div className='login'>
             {/* spotify logo
             Login with spotify button */}
         </div>
-    )
+    );
 }
