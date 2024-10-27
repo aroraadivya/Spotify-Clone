a65190887db1aec0d974a6fe7fb4d80f65f9c5e5
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-10-27 17:25:28 +0530

    Refactor Spotify.js file to update Spotify authentication endpoint and client ID

--- spotify-clone/src/spotify.js
+++ spotify-clone/src/spotify.js
@@ -1,5 +1,5 @@
 export const authEndpoint = "https://accounts.spotify.com/authorize";
 
-const redirectUri = "http://localhost:3000/"
+const redirectUri = "http://localhost:3000/";
 
-const clientId = "039e89525ab4401486d3c31237f6373b"
+const clientId = "039e89525ab4401486d3c31237f6373b";
