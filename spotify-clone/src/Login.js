24db30829ecbb8e82701a7f296bab8a86384e1d1
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-10-27 16:19:24 +0530

    Refactor Login component to remove unnecessary comments and update Spotify logo image path

--- spotify-clone/src/Login.js
+++ spotify-clone/src/Login.js
@@ -6,6 +6,7 @@ function Login() {
         <div className='login'>
             <h1>I'm the login page</h1>
             <img src="https://tse2.mm.bing.net/th?id=OIP.pC9F1MF4XJ0rJuVtd32dAAHaCO&pid=Api&P=0&h=180" alt=""/>
+            
             <button>LOGIN WITH SPOTIFY</button>
         </div>
     );
