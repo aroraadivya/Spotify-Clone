6404edebf4d19954656192ca2aec3c31f41b1afe
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-10-27 15:55:01 +0530

    Refactor Login component to include Spotify logo

--- spotify-clone/src/Login.js
+++ spotify-clone/src/Login.js
@@ -5,6 +5,7 @@ function Login() {
     return (
         <div className='login'>
             <h1>I'm the login page</h1>
+            <img src='https://cdn5.vectorstock.com/i/1000x1000/43/44/spotify-logo-dark-mode-vector-37694344.jpg' alt=''></img>
             {/* spotify logo
             Login with spotify button */}
         </div>
