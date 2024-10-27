13eab31e0c4af7b408e5effafa43a5fbf7903ff7
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-10-27 15:30:42 +0530

    "Added Spotify logo and login button components to App.js"

--- spotify-clone/src/App.js
+++ spotify-clone/src/App.js
@@ -5,6 +5,10 @@ function App() {
     <div className="app">
       <h1>Hello,I'm building spotify clone.</h1>
     </div>
+
+    // Spotify Logo
+    // Login with spotify button
+    <Login />
   );
 }
 
