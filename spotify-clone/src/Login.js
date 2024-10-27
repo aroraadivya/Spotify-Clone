0697b3268dd1c1dbd080836d32755da9151d4f4e
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-10-27 16:29:54 +0530

    Refactor Login component to remove unnecessary h1 tag and update Spotify logo image path

--- spotify-clone/src/Login.js
+++ spotify-clone/src/Login.js
@@ -4,7 +4,6 @@ import './Login.css'
 function Login() {
     return (
         <div className='login'>
-            <h1>I'm the login page</h1>
             <img src="https://tse1.mm.bing.net/th?id=OIP.cZl2awXp3fI5rEyhqoIkdAHaEK&pid=Api&P=0&h=180" alt=""/>
             
             <a>LOGIN WITH SPOTIFY</a>
