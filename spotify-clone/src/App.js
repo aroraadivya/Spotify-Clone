07f8021ebe3363a1b404235834af3991b8108730
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-10-28 21:39:09 +0530

    Update package.json and package-lock.json to add spotify-web-api-js dependency

--- spotify-clone/src/App.js
+++ spotify-clone/src/App.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 import Login from './Login';
 import { getTokenFromUrl } from './spotify';
+import SpotifyWebApi from "spotify-web-api-js";
 
 function App() {
   const [token, setToken] = useState(null);
