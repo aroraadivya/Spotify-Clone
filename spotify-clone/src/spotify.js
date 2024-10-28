d3e406742f39fa6a5e2dad9b6f22a29db6ec59a5
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-10-28 16:09:52 +0530

    Refactor Spotify.js to fix getTokenFromUrl function and update Spotify authentication endpoint, client ID, redirect URI parameter, and add new scopes

--- spotify-clone/src/spotify.js
+++ spotify-clone/src/spotify.js
@@ -22,7 +22,8 @@ export const getTokenFromUrl = () => {
            //accessToken=mysupersecretkey&name=divya
            let parts = item.split('=');
            initial[parts[0]] = decodeURIComponent(parts[1]);
-       })
+           return initial;
+       }, {})
 }
 
 export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join('%20')}&response_type=token&show_dialog=true`;
