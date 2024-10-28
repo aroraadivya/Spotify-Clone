1df097c37507586aa89a46b441d88dbb4d5814c1
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-10-28 16:08:41 +0530

    Refactor Spotify.js to update getTokenFromUrl function to correctly parse the URL hash

--- spotify-clone/src/spotify.js
+++ spotify-clone/src/spotify.js
@@ -18,6 +18,11 @@ export const getTokenFromUrl = () => {
     return window.location.hash
        .substring(1)
        .split('&')
+       .reduce((initial, item) => {
+           //accessToken=mysupersecretkey&name=divya
+           let parts = item.split('=');
+           initial[parts[0]] = decodeURIComponent(parts[1]);
+       })
 }
 
 export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join('%20')}&response_type=token&show_dialog=true`;
