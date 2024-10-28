8f1ed3a5c86182d9b46f40d78a8697a42385410f
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-10-28 16:15:09 +0530

    Refactor App.js to add useEffect hook for running code based on a condition

--- spotify-clone/src/App.js
+++ spotify-clone/src/App.js
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import Login from './Login';
 
 function App() {
+
+  //run code on the basis of given condition
+  useEffect(() => {
+    //code
+  });
+
   return (
       <div className="app">
         <Login />
