417f9d694cae6b8690ef64ad1dcbaad14d058470
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-10-28 14:38:19 +0530

    Refactor Login.js to update Spotify authentication endpoint and add redirect URI parameter

--- spotify-clone/src/Login.js
+++ spotify-clone/src/Login.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './Login.css'
+import { loginUrl } from './spotify';
 
 function Login() {
     return (
         <div className='login'>
             <img src="https://tse1.mm.bing.net/th?id=OIP.cZl2awXp3fI5rEyhqoIkdAHaEK&pid=Api&P=0&h=180" alt=""/>
             
-            <a>LOGIN WITH SPOTIFY</a>
+            <a href={loginUrl}>LOGIN WITH SPOTIFY</a>
         </div>
     );
 }
