0c773ee1b00324d3d6f8acb48a6a6da493d34d6b
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-10-28 20:01:38 +0530

    Refactor App.js to update token retrieval and add token state

--- spotify-clone/src/App.js
+++ spotify-clone/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Login from './Login';
 import { getTokenFromUrl } from './spotify';
@@ -9,12 +9,12 @@ function App() {
   useEffect(() => {
     const hash = getTokenFromUrl();
     window.location.hash = "";
-    const token = hash.access_token;
+    const _token = hash.access_token;
 
-    if (token) {
-      setToken(token);
+    if (_token) {
+      setToken(_token);
     }
-    
+
     console.log("I HAVE A TOKEN", token);
   }, []);
 
