743d1d6c7b8f10c84f16c3d4d235247263047dac
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-10-29 10:52:42 +0530

    Add DataLayer component

--- spotify-clone/src/index.js
+++ spotify-clone/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import DataLayer from './DataLayer';
 
 ReactDOM.render(
   <React.StrictMode>
