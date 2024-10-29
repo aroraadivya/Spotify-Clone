b3266ef8a31e40b11b014b4f9e49c4e16283df31
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-10-29 10:35:11 +0530

    Refactor Login.js to update Spotify logo URL

--- spotify-clone/src/Login.js
+++ spotify-clone/src/Login.js
@@ -5,7 +5,7 @@ import { loginUrl } from './spotify';
 function Login() {
     return (
         <div className='login'>
-            <img src="https://tse1.mm.bing.net/th?id=OIP.cZl2awXp3fI5rEyhqoIkdAHaEK&pid=Api&P=0&h=180" alt=""/>
+            <img src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" alt=""/>
             
             <a href={loginUrl}>LOGIN WITH SPOTIFY</a>
         </div>
