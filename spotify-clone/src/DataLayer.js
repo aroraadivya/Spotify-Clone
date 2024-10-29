ec734c0114c44fa7f2336ad4a1468a6f282b3271
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-10-29 12:29:54 +0530

    Refactor import statements and add missing export in DataLayer.js

--- spotify-clone/src/DataLayer.js
+++ spotify-clone/src/DataLayer.js
@@ -5,3 +5,10 @@ import {
 } from "react";
 
 export const StateContezt = createContext();
+
+export const DataLayer = ({ initialState, reducer, children }) => (
+    
+)
+
+
+    export default DataLayer;
