8fb4c082efc2787d1c31fdf23eb1a7f293e51a99
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-10-29 12:44:39 +0530

    Refactor import statements and add missing export in DataLayer.js

--- spotify-clone/src/reducer.js
+++ spotify-clone/src/reducer.js
@@ -4,3 +4,17 @@ export const initialState = {
     playing: false,
     item: null,
 };
+
+const reducer = (state, action) => {
+    console.log(action);
+
+    switch(action.type){
+        case 'SET_USER':
+            return {
+                ...state,
+                user: action.user
+            }
+    }
+}
+
+export default reducer;
