e1b4446b63ae1022b78d96537e2fbf1b95cf50fb
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-11-02 11:27:53 +0530

    Add comment to clarify action structure in reducer

--- spotify-clone/src/reducer.js
+++ spotify-clone/src/reducer.js
@@ -8,6 +8,7 @@ export const initialState = {
 const reducer = (state, action) => {
     console.log(action);
 
+    //Action -> type, [payload]
     switch(action.type){
         case 'SET_USER':
             return {
