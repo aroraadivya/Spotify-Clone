2ce7364226735e9691490b59f229999ac5a17dfd
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-11-02 12:23:23 +0530

    Refactor App.js to use getTokenFromResponse, streamline token handling, and improve user state management

--- spotify-clone/src/App.js
+++ spotify-clone/src/App.js
@@ -1,57 +1,70 @@
-import React, { useEffect, useState } from 'react';
-import './App.css';
-import Login from './Login';
-import { getTokenFromUrl } from './spotify';
+import React, { useEffect } from "react";
 import SpotifyWebApi from "spotify-web-api-js";
-import Player from './Player';
-import { useDataLayerValue  } from './DataLayer';
+import { useStateValue } from "./StateProvider";
+import Player from "./Player";
+import { getTokenFromResponse } from "./spotify";
+import "./App.css";
+import Login from "./Login";
 
-const spotify = new SpotifyWebApi();
+const s = new SpotifyWebApi();
+
 function App() {
-  // const [token, setToken] = useState(null);
-  const [{user, token }, dispatch] = useDataLayerValue();
+  const [{ token }, dispatch] = useStateValue();
 
   useEffect(() => {
-    const hash = getTokenFromUrl();
+    // Set token
+    const hash = getTokenFromResponse();
     window.location.hash = "";
-    const _token = hash.access_token;
+    let _token = hash.access_token;
 
     if (_token) {
+      s.setAccessToken(_token);
 
       dispatch({
-        type: 'SET_TOKEN',
-        token: _token
+        type: "SET_TOKEN",
+        token: _token,
       });
 
-      spotify.setAccessToken(_token);
-
-      spotify.getMe().then(user => {
-        console.log('User info:', user);
-
+      s.getPlaylist("37i9dQZEVXcJZyENOWUFo7").then((response) =>
         dispatch({
-          type: 'SET_USER',
-          user: user
+          type: "SET_DISCOVER_WEEKLY",
+          discover_weekly: response,
+        })
+      );
+
+      s.getMyTopArtists().then((response) =>
+        dispatch({
+          type: "SET_TOP_ARTISTS",
+          top_artists: response,
+        })
+      );
+
+      dispatch({
+        type: "SET_SPOTIFY",
+        spotify: s,
+      });
+
+      s.getMe().then((user) => {
+        dispatch({
+          type: "SET_USER",
+          user,
+        });
+      });
+
+      s.getUserPlaylists().then((playlists) => {
+        dispatch({
+          type: "SET_PLAYLISTS",
+          playlists,
         });
       });
     }
-
-    console.log("I HAVE A TOKEN", token);
-  }, []);
-
-  console.log('User is', user);
-  console.log('Token is', token);
+  }, [token, dispatch]);
 
   return (
-      <div className="app">
-        {
-          token?(
-            <Player />
-            // <h1>I'm logged in</h1>
-          ) : (
-            <Login />
-          )
-        }
-      </div>
+    <div className="app">
+      {!token && <Login />}
+      {token && <Player spotify={s} />}
+    </div>
   );
 }
 
