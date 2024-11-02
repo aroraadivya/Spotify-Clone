6f9c65b7e34963d7349f331a2758f23b9c0a6cf5
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-11-02 13:09:55 +0530

    Refactor authentication logic and update scopes in Spotify integration

--- spotify-clone/src/spotify.js
+++ spotify-clone/src/spotify.js
@@ -1,31 +1,28 @@
 // https://developer.spotify.com/documentation/web-playback-sdk/quick-start/#
-
 export const authEndpoint = "https://accounts.spotify.com/authorize";
-
-const redirectUri = encodeURIComponent("http://localhost:3000/");
+// Replace with your app's client ID, redirect URI and desired scopes
 const clientId = "039e89525ab4401486d3c31237f6373b";
-
+const redirectUri = encodeURIComponent("http://localhost:3000/");
 const scopes = [
-    "user-read-currently-playing",
-    "user-read-playback-state",
-    "user-modify-playback-state",
-    "user-read-private",
-    "user-top-read",
-    "app-remote-control" 
+  "user-read-currently-playing",
+  "user-read-recently-played",
+  "user-read-playback-state",
+  "user-top-read",
+  "user-modify-playback-state",
 ];
 
-export const getTokenFromUrl = () => {
-    return window.location.hash
-       .substring(1)
-       .split('&')
-       .reduce((initial, item) => {
-           //accessToken=mysupersecretkey&name=divya
-           let parts = item.split('=');
-           initial[parts[0]] = decodeURIComponent(parts[1]);
-           return initial;
-       }, {})
-}
+export const getTokenFromResponse = () => {
+  return window.location.hash
+    .substring(1)
+    .split("&")
+    .reduce((initial, item) => {
+      var parts = item.split("=");
+      initial[parts[0]] = decodeURIComponent(parts[1]);
 
-export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join('%20')}&response_type=token&show_dialog=true`;
+      return initial;
+    }, {});
+};
 
-console.log(loginUrl);
+export const accessUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+  "%20"
+)}&response_type=token&show_dialog=true`;
