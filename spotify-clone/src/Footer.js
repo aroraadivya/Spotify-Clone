fc6d40e71c5053515389b62d532833cf72e34cc9
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-11-02 15:26:47 +0530

    Comment out unused dispatch in Footer component

--- spotify-clone/src/Footer.js
+++ spotify-clone/src/Footer.js
@@ -12,7 +12,7 @@ import PlaylistPlayIcon from "@mui/icons-material/PlaylistPlay";
 import { Grid, Slider } from "@mui/material";
 
 function Footer({ spotify }) {
-  const [{ token, item, playing }, dispatch] = useStateValue();
+  // const [{ token, item, playing }, dispatch] = useStateValue();
 
   useEffect(() => {
     spotify.getMyCurrentPlaybackState().then((r) => {
